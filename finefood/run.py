a019b99ba60e3bc9b7058afe09aeacb1f88a1940
Joel Akeret <joel.akeret@gmail.com>
2018-06-12 16:07:28 +0200

    preparing first run

--- finefood/run.py
+++ finefood/run.py
@@ -55,6 +55,7 @@ def get_tensorboard_callback(kwargs):
     outputs_path = get_outputs_path()
     if outputs_path is None:
         outputs_path = "./output"
+    print("Storing tensorflow logs to", outputs_path)
 
     tensorboard = TensorBoard(log_dir=outputs_path,
                               histogram_freq=0,
@@ -68,8 +69,9 @@ def get_checkpoint_callback():
     outputs_path = get_outputs_path()
     if outputs_path is None:
         outputs_path = "./output"
-
     filepath = os.path.join(outputs_path, filename)
+    print("Storing checkpoints to", filepath)
+
     checkpoint = ModelCheckpoint(filepath,
                                  monitor='val_acc',
                                  verbose=1,
@@ -78,12 +80,12 @@ def get_checkpoint_callback():
     return checkpoint
 
 
-def launch(epochs, batch_size, learning_rate, dropout):
+def launch(epochs, batch_size, learning_rate, dropout, sample_size=None):
     max_len = 10
 
     corpus, word_to_index, word_to_vec_map = utils.read_glove_vecs("./data/glove.6B/glove.6B.50d.txt")
 
-    X_test, X_train, y_test, y_train = load_data(corpus, max_len, word_to_index)
+    X_test, X_train, y_test, y_train = load_data(corpus, max_len, word_to_index, sample_size)
 
     model = score_model.build_model((max_len,), dropout, word_to_vec_map, word_to_index)
     optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, decay=0.01)
@@ -93,25 +95,36 @@ def launch(epochs, batch_size, learning_rate, dropout):
     history = train_model(model, X_train, X_test, y_train, y_test,
                           epochs=epochs, batch_size=batch_size, shuffle=True)
 
-    send_metrics(loss=history.history["loss"][-1],
-                 val_loss=history.history["val_loss"][-1],
-                 accuracy=history.history["acc"][-1],
-                 val_accuracy=history.history["val_acc"][-1])
+    try:
+        send_metrics(loss=history.history["loss"][-1],
+                     val_loss=history.history["val_loss"][-1],
+                     accuracy=history.history["acc"][-1],
+                     val_accuracy=history.history["val_acc"][-1])
+    except Exception:
+        pass
 
 
-def load_data(corpus, max_len, word_to_index):
+def load_data(corpus, max_len, word_to_index, sample_size):
     data_path = get_data_path()
     if data_path is None:
-        data_path = "../data"
+        data_path = "./data"
     path = os.path.join(data_path, "Reviews.csv")
 
+    print("Loading data from", path)
+
     df = pd.read_csv(path).set_index("Id")
-    df = df.sample(n=1000)
+    if sample_size is None:
+        sample_size = df.size
+
+    df = df.sample(n=sample_size)
+
     clean_texts = np.array([preprocessing.clean_text(t, corpus) for t in df.Text])
     scores = df.Score.values
     scores_oh = np_utils.to_categorical(scores)[:, 1:]
     text_indices = preprocessing.sentences_to_indices(clean_texts, word_to_index, max_len)
     X_train, X_test, y_train, y_test = train_test_split(text_indices, scores_oh, test_size=.2)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
     return X_test, X_train, y_test, y_train
 
 
@@ -142,9 +155,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     arguments = args.__dict__
 
-    batch_size = arguments.pop('batch_size')
-    learning_rate = arguments.pop('learning_rate')
-    dropout = arguments.pop('dropout')
-    num_epochs = arguments.pop('num_epochs')
-
-    launch(num_epochs, batch_size, learning_rate, dropout)
+    launch(arguments.pop('num_epochs'),
+           arguments.pop('batch_size'),
+           arguments.pop('learning_rate'),
+           arguments.pop('dropout'))
