0bd6453470fb43ddeb6c95b7435a7cc9f73d7bc5
Joel Akeret <joel.akeret@gmail.com>
2018-06-23 14:04:25 +0200

    one layer cnn

--- finefood/score_model.py
+++ finefood/score_model.py
@@ -4,24 +4,23 @@ import h5py
 import numpy as np
 
 from keras.models import Model
-from keras.layers import Dense, Input, Dropout, LSTM, Activation, Conv1D, MaxPooling1D, Flatten
+from keras.layers import Dense, Input, Dropout, LSTM, Activation, Conv1D, MaxPooling1D, Flatten, merge, Concatenate
 from keras.layers import Embedding
 from polyaxon_helper import get_data_path
 
+def build_model(model_type, max_len, dropout, trainable_embbedings):
+    MODEL_TYPE_MAP = {
+        "lstm": build_lstm_model,
+        "2layer_lstm": build_2layer_lstm_model,
+        "cnn_lstm": build_cnn_lstm_model,
+        "cnn": build_cnn_model,
+        "1layer_cnn": build_1layer_cnn_model
+    }
 
-def build_model(model_type, max_len, dropout):
-    if model_type == "2layer_lstm":
-        model = build_2layer_lstm_model((max_len,), dropout)
-    elif model_type == "cnn_lstm":
-        model = build_cnn_lstm_model((max_len,), dropout)
-    elif model_type == "cnn":
-        model = build_cnn_model((max_len,), dropout)
-    else:
-        raise "Unknown model: %s"%model_type
+    build_func = MODEL_TYPE_MAP[model_type]
+    return build_func((max_len,), dropout, trainable_embbedings)
 
-    return model
-
-def pretrained_embedding_layer(max_len):
+def pretrained_embedding_layer(max_len, trainable=True):
     """
     Creates a Keras Embedding() layer and loads in pre-trained GloVe 50-dimensional vectors.
 
@@ -42,16 +41,31 @@ def pretrained_embedding_layer(max_len):
     with h5py.File(path, "r") as fp:
         embedding_matrix = fp["embedding_matrix"].value
 
-    # Define Keras embedding layer with the correct output/input sizes, make it trainable. Use Embedding(...). Make sure to set trainable=False.
+    # Define Keras embedding layer with the correct output/input sizes, make it trainable.
     embedding_layer = Embedding(embedding_matrix.shape[0],
                                 embedding_matrix.shape[1],
                                 weights=[embedding_matrix],
                                 input_length=max_len,
-                                trainable=False)
+                                trainable=trainable)
 
     return embedding_layer
 
-def build_2layer_lstm_model(input_shape, dropout):
+def build_lstm_model(input_shape, dropout, trainable_embbedings):
+    sentence_indices = Input(shape=input_shape, dtype=np.int32)
+
+    embedding_layer =  pretrained_embedding_layer(input_shape[0], trainable_embbedings)
+
+    embeddings = embedding_layer(sentence_indices)
+
+    X = LSTM(128)(embeddings)
+    X = Dropout(dropout)(X)
+    X = Dense(5, activation='softmax')(X)
+
+    model = Model(sentence_indices, X)
+
+    return model
+
+def build_2layer_lstm_model(input_shape, dropout, trainable_embbedings):
     """
     Function creating the model's graph.
 
@@ -68,7 +82,7 @@ def build_2layer_lstm_model(input_shape, dropout):
     sentence_indices = Input(shape=input_shape, dtype=np.int32)
 
     # Create the embedding layer pretrained with GloVe Vectors (≈1 line)
-    embedding_layer =  pretrained_embedding_layer(input_shape[0])
+    embedding_layer =  pretrained_embedding_layer(input_shape[0], trainable_embbedings)
 
     # Propagate sentence_indices through your embedding layer, you get back the embeddings
     embeddings = embedding_layer(sentence_indices)
@@ -91,10 +105,10 @@ def build_2layer_lstm_model(input_shape, dropout):
 
     return model
 
-def build_cnn_lstm_model(input_shape, dropout):
+def build_cnn_lstm_model(input_shape, dropout, trainable_embbedings):
     sentence_indices = Input(shape=input_shape, dtype=np.int32)
 
-    embedding_layer =  pretrained_embedding_layer(input_shape[0])
+    embedding_layer =  pretrained_embedding_layer(input_shape[0], trainable_embbedings)
 
     # Propagate sentence_indices through your embedding layer, you get back the embeddings
     embeddings = embedding_layer(sentence_indices)
@@ -108,9 +122,9 @@ def build_cnn_lstm_model(input_shape, dropout):
 
     return model
 
-def build_cnn_model(input_shape, dropout):
+def build_cnn_model(input_shape, dropout, trainable_embbedings):
     sequence_input = Input(shape=input_shape, dtype='int32')
-    embedding_layer = pretrained_embedding_layer(input_shape[0])
+    embedding_layer = pretrained_embedding_layer(input_shape[0], trainable_embbedings)
     embedded_sequences = embedding_layer(sequence_input)
     x = Conv1D(128, 5, activation='relu')(embedded_sequences)
     x = MaxPooling1D(5)(x)
@@ -120,6 +134,30 @@ def build_cnn_model(input_shape, dropout):
     x = MaxPooling1D(35)(x)  # global max pooling
     x = Flatten()(x)
     x = Dense(128, activation='relu')(x)
+    preds = Dense(5, activation='softmax')(x)
+
+    model = Model(sequence_input, preds)
+    return model
+
+
+def build_1layer_cnn_model(input_shape, dropout, trainable_embbedings):
+    sequence_input = Input(shape=input_shape, dtype='int32')
+    max_len = input_shape[0]
+    embedding_layer = pretrained_embedding_layer(max_len, trainable_embbedings)
+
+    embedded_sequences = embedding_layer(sequence_input)
+    feature_maps = []
+    for filter_region_size in (5,6,7):
+        x1 = Conv1D(100, filter_region_size, activation='relu')(embedded_sequences)
+        x1 = MaxPooling1D(max_len - filter_region_size)(x1)
+        feature_maps.append(x1)
+
+
+    x = Concatenate()(feature_maps)
+    x = Dropout(dropout)(x)
+    # x = MaxPooling1D(6)(x)
+    x = Flatten()(x)
+
     preds = Dense(5, activation='softmax')(x)
 
     model = Model(sequence_input, preds)
